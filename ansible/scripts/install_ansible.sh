256e416157466ad03b20439cfcb31c27f2892465
Arvind Shyamsundar <arvindsh@microsoft.com>
2023-10-27 15:18:16 -0700

    Add support for Alma and Rocky Linux

--- ansible/scripts/install_ansible.sh
+++ ansible/scripts/install_ansible.sh
@@ -24,7 +24,7 @@ set -e
 
 # enable yum epel repo
 os_id=$(grep '^ID=' /etc/os-release | cut -d'=' -f2 | tr -d '"')
-if [[ $os_id = "centos" ]]; then
+if [[ $os_id = "centos" || $os_id = "almalinux" || $os_id = "rocky" ]]; then
   is_installed_epel_release="rpm -q --quiet epel-release"
   install_epel_release="sudo yum install -q -y epel-release"
   for i in {1..10}; do ($is_installed_epel_release || $install_epel_release) && break || sleep 15; done
@@ -54,7 +54,8 @@ fi
 
 # install lxml as it is a dependency for the maven_artifact Ansible module
 centos_version=$(grep '^VERSION_ID=' /etc/os-release | cut -d'=' -f2 | tr -d '"')
-if [[ $os_id = "centos" ]]; then
+centos_version=${centos_version%.*}
+if [[ $os_id = "centos" || $os_id = "almalinux" || $os_id = "rocky" ]]; then
   if [[ $centos_version -eq 7 ]]; then
      sudo yum install -q -y python-lxml
   elif [[ $centos_version -eq 8 ]]; then
